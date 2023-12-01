74207cd0fe7854fd83e66725bfbfb4c452583566
Harperzhu <harperzhu@yahoo.com>
2023-12-01 17:09:12 -0500

    building neural network

--- Model.py
+++ Model.py
@@ -1,12 +1,12 @@
-from sklearn.model_selection import train_test_split
-from sklearn.ensemble import AdaBoostRegressor
-from sklearn.model_selection import cross_val_score
-from sklearn.preprocessing import LabelEncoder
-from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
-from sklearn.metrics import mean_squared_error
 import pandas as pd
-from sklearn.tree import DecisionTreeRegressor  # Import the DecisionTreeRegressor
 import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.compose import ColumnTransformer
+from keras.layers import Dense
+from keras.models import Sequential
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
+
 
 # Load and preprocess data
 merged_data = pd.read_csv('DataSet/merged_pet_claims_data.csv')
@@ -17,43 +17,68 @@ def process_age(age):
 
 merged_data['PetAge'] = merged_data['PetAge'].apply(process_age)
 
-# Handle categorical variables
-label_encoder_species = LabelEncoder()
-label_encoder_breed = LabelEncoder()
-merged_data['Species'] = label_encoder_species.fit_transform(merged_data['Species'])
-merged_data['Breed'] = label_encoder_breed.fit_transform(merged_data['Breed'])
+# Assuming 'PetAge' is a numerical feature (modify according to your dataset)
+numerical_features = ['PetAge']
+categorical_features = ['Species', 'Breed']  # Assuming these are categorical
 
-# Define features and target
+
+# Define your features and target variable
 features = ['PetAge', 'Species', 'Breed']
 target = 'Premium'
 
-X = merged_data[features]
+# Define your features and target variable
+target = 'Premium'
+
+# Preprocessing for numerical and categorical data
+preprocessor = ColumnTransformer(
+    transformers=[
+        ('num', StandardScaler(), numerical_features),
+        ('cat', OneHotEncoder(handle_unknown='ignore'), categorical_features)
+    ])
+
+# Prepare the data
+X = merged_data[numerical_features + categorical_features]
 y = merged_data[target]
 
-base_estimator = DecisionTreeRegressor(max_depth=10) 
 
+# Split the dataset into training and test sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-ada_reg = AdaBoostRegressor(base_estimator=base_estimator, n_estimators=100, random_state=42)
+
+
+# Apply preprocessing
+X_train = preprocessor.fit_transform(X_train)
+X_test = preprocessor.transform(X_test)
+
+# Convert to NumPy arrays if necessary
+X_train = X_train.toarray() if hasattr(X_train, 'toarray') else X_train
+X_test = X_test.toarray() if hasattr(X_test, 'toarray') else X_test
+
+# Neural network model
+model = Sequential()
+model.add(Dense(128, activation='relu', input_shape=(X_train.shape[1],)))
+model.add(Dense(64, activation='relu'))
+model.add(Dense(1))  # Output layer for regression
+
+# Compile the model
+model.compile(optimizer='adam', loss='mean_squared_error')
 
 # Train the model
-ada_reg.fit(X_train, y_train)
+model.fit(X_train, y_train, epochs=40, batch_size=32)
 
 # Make predictions and evaluate the model
-predictions = ada_reg.predict(X_test)
+predictions = model.predict(X_test)
+# Check if y_test contains NaN values
+print(f"NaN in y_test: {y_test.isna().any()}")
 
+# # Since 'predictions' is a numpy array returned by the model, you can check for NaNs like this:
+# print(f"NaN in predictions: {np.isnan(predictions).any()}")
 
-# Perform cross-validation
-cv_scores = cross_val_score(ada_reg, X, y, cv=5, scoring='neg_mean_squared_error')
+# mse = mean_squared_error(y_test, predictions)
+# rmse = np.sqrt(mse)
+# mae = mean_absolute_error(y_test, predictions)
+# r2 = r2_score(y_test, predictions)
 
-# Convert scores to positive values (since they are negative MSE)
-mse_scores = -cv_scores
-
-mse = mean_squared_error(y_test, predictions)
-rmse = np.sqrt(mse)
-mae = mean_absolute_error(y_test, predictions)
-r2 = r2_score(y_test, predictions)
-
-print(f"Mean Squared Error: {mse}")
-print(f"Root Mean Squared Error: {rmse}")
-print(f"Mean Absolute Error: {mae}")
-print(f"R-squared: {r2}")
+# print(f"Mean Squared Error: {mse}")
+# print(f"Root Mean Squared Error: {rmse}")
+# print(f"Mean Absolute Error: {mae}")
+# print(f"R-squared: {r2}")
